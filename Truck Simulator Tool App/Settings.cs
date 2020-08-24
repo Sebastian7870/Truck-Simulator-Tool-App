026b73066a041248c6af3b4770934288966b3b03
Sebastian7870 <sebastian.mannheim.04@gmail.com>
2020-08-24 19:54:56 +0200

    -Added basic controls (Ip address, alerts) -Added Settings class -(I) Learned many things

---
+++ Truck Simulator Tool App/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace Truck_Simulator_Tool_App
+{
+    public class Settings
+    {
+        public string TSTServerIP { get; set; }
+    }
+}
--- Truck Simulator Tool App/TST_ServerData.cs
+++ Truck Simulator Tool App/TST_ServerData.cs
@@ -2,5 +2,6 @@
 {
     class TST_ServerData
     {
+
     }
 }
